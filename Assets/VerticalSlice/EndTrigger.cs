34d02f806231e66bee30845661806ed08034f258
Gerald Schenke <realazrael@gmx.de>
2017-05-24 17:15:16 +0200

    Chapter 14 - Vertical Slice

---
+++ Assets/VerticalSlice/EndTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour {
+	public string scenename = "EndScreen";
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {			
+			SceneManager.LoadScene (scenename);
+		}
+	}
+}
